f8ae56e1c26df084b3cf7920a078fc89a95c73d3
AFLAH <aflah.skybook@gmail.com>
2022-10-05 17:16:04 +0530

    delete note setup

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import getNote, getNotes, getRoutes
+from .views import getNote, getNotes, getRoutes, updateNote, deleteNote
 
 urlpatterns = [
     path('', getRoutes, name='routes'),
     path('notes/', getNotes, name='notes'),
-    path('notes/<str:pk>', getNote, name='note'),
+    path('notes/<str:pk>/update/', updateNote, name='update-note'),
+    path('notes/<str:pk>/delete/', deleteNote, name='delete-note'),
+    path('notes/<str:pk>/', getNote, name='note'),
 ]
